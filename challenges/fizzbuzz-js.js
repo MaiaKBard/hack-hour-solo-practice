1b0c0e12b6b5e455f24313b87fc5f31b7bf76de6
Maia <Maiakbard@gmail.com>
2026-02-04 16:14:33 -0600

    Add problems for FizzBuzz without pair programming work

--- challenges/fizzbuzz-js.js
+++ challenges/fizzbuzz-js.js
@@ -1,27 +1,4 @@
-  // //create an output array 
-  // const output = []
-
-  // //create count variable starting at the input number 
-  // let count = 0
-
-  // //iterate the count variable - decrementing until equivalent to 0
-  // while(count <= num){
-  //   //if number we are accessing is divisible by 3 NOT 5 push "fizz" into output array
-  //   if(count % 3 === 0 && count % 5 !== 0) output.push("fizz")
-  //   //if number is divisible by 5 NOT 3 push "buzz" into output array
-  //   else if(count % 5 === 0 && count % 3 !== 0) output.push("buzz")
-  //   //if number is divisible by BOTH 3 and 5 push 'fizzbuzz' into the output array
-  //   else if(count % 5 === 0  && count % 3 === 0) output.push("fizzbuzz")
-  //   //otherwise push the number we are accessing
-  //   else(output.push(count))
-  //   //decremnt the count varible
-  //   count++
-  // }
-    
-
-  // //return output array 
-  // return output
-/*
+  /*
 
 Write a function that returns an array containing the numbers 1 to NUM.
 
@@ -35,31 +12,11 @@ fizzbuzz(16);
 
 */
 
-//create a function fizzbuzz //input: number //output: array
 const fizzbuzz = (num) => {
-
-  //create an output array
-  const output = [];
-  // create a for loop to iterate through num
-  for (let i = 1; i <= num; i++) {
-  // create a dynamic string, use compound assignment to add to it based on conditions being met (or an array)
-  const currentWord = []; //let myString = '';
-  // check for 3
-  if (i % 3 === 0) currentWord.push('fizz'); //myString += 'fizz';
-  // check for 5
-  if (i % 5 === 0) currentWord.push('buzz'); //myString += 'buzz';
-  //push the resulting accumulated string into the output array
-  output.push(currentWord.join('') || i); //output.push(myString || i)
-  } //close loop
-
-  return output;
-  //return an output array
+  return [];
 };
 
-
-
 /*
-
 
 Extension: 
 Write a function that returns an array containing the numbers 1 to NUM.
@@ -80,23 +37,7 @@ fizzbuzzbazz(25);
 */
 
 const fizzbuzzbazz = (num) => {
-    //create an output array
-  const output = [];
-  // create a for loop to iterate through num
-  for (let i = 1; i <= num; i++) {
-  // create a dynamic string, use compound assignment to add to it based on conditions being met (or an array)
-  const currentWord = []; //let myString = '';
-  // check for 3
-  if (i % 3 === 0) currentWord.push('fizz'); //myString += 'fizz';
-  // check for 5
-  if (i % 5 === 0) currentWord.push('buzz'); //myString += 'buzz';
-  // check for 7
-  if (i % 7 === 0) currentWord.push('bazz'); //myString += 'bazz';  //push the resulting accumulated string into the output array
-  output.push(currentWord.join('') || i); //output.push(myString || i)
-  } //close loop
-
-  return output;
-  //return an output array
+  return [];
 };
 
 export { fizzbuzz, fizzbuzzbazz };
